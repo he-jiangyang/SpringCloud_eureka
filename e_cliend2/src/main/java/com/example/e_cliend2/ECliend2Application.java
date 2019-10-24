52345aadbf5ec4f8255026ddc84176fd4248f15c
he-jiangyang <1270360524@qq.com>
2019-10-24 10:10:49 +0800

    222

--- e_cliend2/src/main/java/com/example/e_cliend2/ECliend2Application.java
+++ e_cliend2/src/main/java/com/example/e_cliend2/ECliend2Application.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class ECliend2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ECliend2Application.class, args);
     }
 
